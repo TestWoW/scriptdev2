59b6cceef32a35a62e2d74b54b58d00d86635915
Walkum <juanmavictoria@gmail.com>
2011-11-26 22:07:33 +0100

    Arraglados fallos con los conflicts en Ahn'kahet 2 (despiste)

--- scripts/northrend/azjol-nerub/ahnkahet/ahnkahet.h
+++ scripts/northrend/azjol-nerub/ahnkahet/ahnkahet.h
@@ -1,5 +1,4 @@
 /* Copyright (C) 2006 - 2011 ScriptDev2 <http://www.scriptdev2.com/>
- * Copyright (C) 2011 MangosR2
  * This program is free software licensed under GPL version 2
  * Please see the included DOCS/LICENSE.TXT for more information */
 
@@ -27,14 +26,47 @@ enum
     GO_ANCIENT_DEVICE_R     = 193094,
     GO_VORTEX               = 193564,
 
-    NPC_ELDER_NADOX               = 29309,
-    NPC_AHNKAHAR_GUARDIAN_EGG     = 30173,
-    NPC_AHNKAHAR_SWARM_EGG        = 30172,
-    NPC_AHNKAHAR_GUARDIAN         = 30176,
-    NPC_AHNKAHAR_SWARMER          = 30178,
-
+    NPC_ELDER_NADOX         = 29309,
     NPC_TALDARAM            = 29308,
     NPC_JEDOGA_SHADOWSEEKER = 29310,
+    NPC_TWILIGHT_INITIATE   = 30114,
+
+
+    ACHIEV_CRITERIA_VOLUNTEER_WORK      = 7359,
+    ACHIEV_CRITERIA_RESPECT_YOUR_ELDERS = 7317
+};
+
+class MANGOS_DLL_DECL instance_ahnkahet : public ScriptedInstance
+{
+    public:
+        instance_ahnkahet(Map* pMap);
+        ~instance_ahnkahet() {}
+
+        void Initialize();
+
+        void OnCreatureCreate(Creature* pCreature);
+        void OnObjectCreate(GameObject* pGo);
+
+        void SetData(uint32 uiType, uint32 uiData);
+        void SetAchiev(uint32 uiType, bool get);
+        uint32 GetData(uint32 uiType);
+
+        bool CheckAchievementCriteriaMeet(uint32 uiCriteriaId, Player const* pSource, Unit const* pTarget, uint32 uiMiscValue1 /* = 0*/);
+
+        const char* Save() { return strInstData.c_str(); }
+        void Load(const char* chrIn);
+
+        GUIDList m_lTwilightInitiate;
+
+
+    protected:
+        uint32 m_auiEncounter[MAX_ENCOUNTER];
+        std::string strInstData;
+
+        bool   m_bCriteriaVolunteerWork;
+        bool   m_bCriteriaRespectYourElders;
+
+        uint8  m_uiDevicesActivated;
 };
 
 #endif
