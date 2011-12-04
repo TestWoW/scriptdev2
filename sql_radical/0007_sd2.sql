c12b14ce15383e8d233d48880a92bd9b1cbd55c6
Walkum <juanmavictoria@gmail.com>
2011-12-04 06:07:20 +0100

    [S0060] Fixed quest 13047: "The Reckoning"

---
+++ sql_radical/0007_sd2.sql
@@ -0,0 +1,58 @@
+DELETE FROM `script_texts` WHERE `entry` BETWEEN -1090514 AND -1090500;
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090500, 'It is time I put an end to my brother\'s madness.', NULL, NULL, NULL, NULL, NULL, 'Ya es hora de que ponga punto final a la locura de mi hermano.', 'Ya es hora de que ponga punto final a la locura de mi hermano.', NULL, 0, 0, 0, 0, 'EVENT_01_THORIM');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090501, 'Loken!!', NULL, NULL, NULL, NULL, NULL, '¡Loken!', '¡Loken!', NULL, 0, 1, 0, 0, 'EVENT_02_THORIM');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090502, 'Stand up and fight me, you worthless traitor!', NULL, NULL, NULL, NULL, NULL, '¡Levántate y lucha contra mí, despreciable traidor!', '¡Levántate y lucha contra mí, despreciable traidor!', NULL, 0, 1, 0, 0, 'EVENT_03_THORIM');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090503, 'You seem eager to join your beloved Sif, brother.', NULL, NULL, NULL, NULL, NULL, 'Pareces deseoso de unirte a tu amada Sif, hermano.', 'Pareces deseoso de unirte a tu amada Sif, hermano.', NULL, 0, 0, 0, 0, 'EVENT_04_LOKEN');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090504, 'Do not dare...', NULL, NULL, NULL, NULL, NULL, 'Ni se te ocurra...', 'Ni se te ocurra...', NULL, 0, 0, 0, 0, 'EVENT_05_THORIM');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090505, 'Speak...', NULL, NULL, NULL, NULL, NULL, 'Pronunciar...', 'Pronunciar...', NULL, 0, 0, 0, 0, 'EVENT_06_THORIM');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090506, 'Her name!!', NULL, NULL, NULL, NULL, NULL, '¡Su nombre!', '¡Su nombre!', NULL, 0, 0, 0, 0, 'EVENT_07_THORIM');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090507, 'ENOUGH!', NULL, NULL, NULL, NULL, NULL, '¡BASTA!', '¡BASTA!', NULL, 0, 1, 0, 0, 'EVENT_08_LOKEN');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090508, 'Looks like you can still best me in a fair fight, little brother.', NULL, NULL, NULL, NULL, NULL, 'Parece que todavía puedes ganarme en una batalla justa, hermanito.', 'Parece que todavía puedes ganarme en una batalla justa, hermanito.', NULL, 0, 0, 0, 0, 'EVENT_09_LOKEN');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090509, 'Unfortunately for you.... this fight is anything but fair!', NULL, NULL, NULL, NULL, NULL, 'Por desgracia para ti... ¡esta pelea es de todo menos justa!', 'Por desgracia para ti... ¡esta pelea es de todo menos justa!', NULL, 0, 0, 0, 0, 'EVENT_10_LOKEN');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090510, 'Thanks to your little friend, you\'ve left the Temple of Storms, where you were at your strongest. Instead we fight closer to my master\'s power.', NULL, NULL, NULL, NULL, NULL, 'Gracias a tu pequeño amigo, has abandonado el Templo de las Tormentas, donde eras más fuerte. En cambio, luchamos más cerca del poder de mi maestro.', 'Gracias a tu pequeño amigo, has abandonado el Templo de las Tormentas, donde eras más fuerte. En cambio, luchamos más cerca del poder de mi maestro.', NULL, 0, 0, 0, 0, 'EVENT_11_LOKEN');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090511, 'Minions! Take my brother and his drake to their new dwellings. Make sure the beast makes it alive. I have special plans for the broodmother.', NULL, NULL, NULL, NULL, NULL, '¡Esbirros! Llevad a mi hermano y a su draco a sus nuevos aposentos. Aseguraos de que la bestia siga con vida. Tengo planes especiales para la madre de linaje.', '¡Esbirros! Llevad a mi hermano y a su draco a sus nuevos aposentos. Aseguraos de que la bestia siga con vida. Tengo planes especiales para la madre de linaje.', NULL, 0, 0, 0, 0, 'EVENT_12_LOKEN');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090512, 'As for your life, mortal. I will be generous. After all... why would I destroy my most useful servant? I waited for you for weeks inside that Hyldnir mine.', NULL, NULL, NULL, NULL, NULL, 'Respecto a tu vida, mortal, seré generoso. Después de todo... ¿por qué iba a destruir a mi más útil sirviente? Llevo semanas esperándote dentro de esa mina Hyldnir.', 'Respecto a tu vida, mortal, seré generoso. Después de todo... ¿por qué iba a destruir a mi más útil sirviente? Llevo semanas esperándote dentro de esa mina Hyldnir.', NULL, 0, 0, 0, 0, 'EVENT_13_LOKEN');
+
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`)
+VALUES
+	(-1090513, 'The shape wasn\'t mine, of course. And had you not been so reckless, you would\'ve seen past my illusion. But you came through for me, and for that... I must thank you!', NULL, NULL, NULL, NULL, NULL, 'La forma no era mía, por supuesto. Y si no hubieras sido tan temerario, habrías visto más allá de mi ilusión. Pero viniste a por mí, y por eso debo darte las gracias.', 'La forma no era mía, por supuesto. Y si no hubieras sido tan temerario, habrías visto más allá de mi ilusión. Pero viniste a por mí, y por eso debo darte las gracias.', NULL, 0, 0, 0, 0, 'EVENT_14_LOKEN');
+
