6075b1f531ee98e736f7e3e8085beb0a56c893f6
Raynar RS <davis87@correo.ugr.es>
2011-12-10 18:30:09 +0100

    Twin Valkyr - set max health on reset. by michalpolko

--- scripts/northrend/crusaders_coliseum/trial_of_the_crusader/boss_twin_valkyr.cpp
+++ scripts/northrend/crusaders_coliseum/trial_of_the_crusader/boss_twin_valkyr.cpp
@@ -89,6 +89,7 @@ struct MANGOS_DLL_DECL boss_fjolaAI : public BSWScriptedAI
         m_creature->SetRespawnDelay(7*DAY);
         m_pInstance->SetData(DATA_CASTING_VALKYRS, SPELL_NONE);
         stage = 0;
+	 m_creature->SetHealth(m_creature->GetMaxHealth());
     }
 
     void JustReachedHome()
@@ -249,6 +250,7 @@ struct MANGOS_DLL_DECL boss_eydisAI : public BSWScriptedAI
         m_creature->SetRespawnDelay(7*DAY);
         m_pInstance->SetData(DATA_CASTING_VALKYRS, SPELL_NONE);
         stage = 0;
+	 m_creature->SetHealth(m_creature->GetMaxHealth());
     }
 
     void JustReachedHome()
