a37ffc6330ae600445482755a8694c29b9c65322
Jordi <gascart@gmail.com>
2012-01-24 14:19:16 +0100

    toc

--- scripts/northrend/crusaders_coliseum/trial_of_the_crusader/boss_twin_valkyr.cpp
+++ scripts/northrend/crusaders_coliseum/trial_of_the_crusader/boss_twin_valkyr.cpp
@@ -161,8 +161,8 @@ struct MANGOS_DLL_DECL boss_fjolaAI : public ScriptedAI
         SetEquipmentSlots(false, EQUIP_MAIN_1, EQUIP_OFFHAND_1, EQUIP_RANGED_1);
 
         m_creature->SetRespawnDelay(7*DAY);
-        m_pInstance->SetData(DATA_CASTING_VALKYRS, SPELL_NONE);
         m_creature->SetHealth(m_creature->GetMaxHealth());
+        m_creature->SetByteValue(UNIT_FIELD_BYTES_1, 3, UNIT_BYTE1_FLAG_ALWAYS_STAND | UNIT_BYTE1_FLAG_UNK_2);
     }
 
     void JustReachedHome()
@@ -171,7 +171,6 @@ struct MANGOS_DLL_DECL boss_fjolaAI : public ScriptedAI
             return;
 
         m_pInstance->SetData(TYPE_VALKIRIES, FAIL);
-        m_pInstance->SetData(DATA_CASTING_VALKYRS, SPELL_NONE);
         m_pInstance->SetData(DATA_HEALTH_FJOLA, m_creature->GetMaxHealth());
         m_creature->ForcedDespawn();
     }
@@ -440,8 +439,8 @@ struct MANGOS_DLL_DECL boss_eydisAI : public ScriptedAI
         SetEquipmentSlots(false, EQUIP_MAIN_2, EQUIP_OFFHAND_2, EQUIP_RANGED_2);
 
         m_creature->SetRespawnDelay(7*DAY);
-        m_pInstance->SetData(DATA_CASTING_VALKYRS, SPELL_NONE);
         m_creature->SetHealth(m_creature->GetMaxHealth());
+        m_creature->SetByteValue(UNIT_FIELD_BYTES_1, 3, UNIT_BYTE1_FLAG_ALWAYS_STAND | UNIT_BYTE1_FLAG_UNK_2);
     }
 
     void JustReachedHome()
@@ -450,7 +449,6 @@ struct MANGOS_DLL_DECL boss_eydisAI : public ScriptedAI
             return;
 
         m_pInstance->SetData(TYPE_VALKIRIES, FAIL);
-        m_pInstance->SetData(DATA_CASTING_VALKYRS, SPELL_NONE);
         m_pInstance->SetData(DATA_HEALTH_EYDIS, m_creature->GetMaxHealth());
         m_creature->ForcedDespawn();
     }
